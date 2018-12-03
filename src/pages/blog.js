3e5b5018b37807d3317a4475e1bf618ae6e41209
brady <mac@macs-MacBook-Pro.local>
2018-12-02 18:16:52 -0600

    Added blog page query with simple card styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,16 +7,43 @@ import Container from '../components/Container'
 import CardList from '../components/CardList'
 import Card from '../components/Card'
 
-export default () => (
-   <Layout>
-      <Helmet>
-         <title>{`Blog - ${config.siteTitle}`}</title>
-      </Helmet>
-   <Container>
-         <h1>This is the Blog!</h1>
-         <div className={styles.test}>
-            <p className={styles.test__child}>CSS Modules are cool</p>
-         </div>
-   </Container>
-   </Layout>
-)
+export default ({data, pageContext}) => {
+   const posts = data.allContentfulPost.edges 
+   
+   return (
+      <Layout>
+         <Helmet>
+            <title>{`Blog - ${config.siteTitle}`}</title>
+         </Helmet>
+      <Container>
+            <h1>This is the Blog!</h1>
+            <div className={styles.test}>
+               <p className={styles.test__child}>CSS Modules are cool</p>
+            </div>
+            <div className={styles.cardGroup}>
+               {posts.map(({ node: post }) => (
+                  <div className={styles.card}> {post.title} </div>
+               ))}
+            </div>
+      </Container>
+      </Layout>
+   )
+}
+
+export const query = graphql`
+   {
+      allContentfulPost(
+         sort: { fields: [publishDate], order: DESC }
+         limit: 20
+      ) {
+         edges {
+            node {
+               title
+               id
+               slug
+               publishDate
+            }
+         }
+      }
+   }
+`
