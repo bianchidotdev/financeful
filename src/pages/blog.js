9ba942b4972adb8765b92cd4d2ba555c83b82119
lambertbrady <lambertbrady14@gmail.com>
2018-12-20 17:01:52 -0600

    Added variables.scss util for sharing js and scss variables, with example use case added to blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,19 +6,21 @@ import Layout from '../components/Layout'
 import Container from '../components/Container'
 import CardList from '../components/CardList'
 import Card from '../components/Card'
+import variables from '../utils/variables.scss';
 
 export default ({data, pageContext}) => {
    const posts = data.allContentfulPost.edges 
-   
+   console.log('testing');
+   console.log(variables.colorTest);
    return (
       <Layout>
          <Helmet>
             <title>{`Blog - ${config.siteTitle}`}</title>
          </Helmet>
       <Container>
-            <h1>This is the Blog!</h1>
+            <h1 style={{color: variables.colorTest}}>This header is styled with a js variable.</h1>
             <div className={styles.test}>
-               <p className={styles.test__child}>CSS Modules are cool</p>
+               <p className={styles.test__child}>This background is styled with the same variable, but used in scss.</p>
             </div>
             <div className={styles.cardGroup}>
                {posts.map(({ node: post }) => (
